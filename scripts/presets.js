2d9a73e87e99ec7abec48befea628b56091d7e80
Elias Faisal <60409013+eliasfaisal@users.noreply.github.com>
2022-05-22 20:44:15 +0000

    Add files via upload

---
+++ scripts/presets.js
@@ -0,0 +1,49 @@
+function $(s) {
+    return document.querySelector(s);
+}
+
+function $$(s) {
+    return document.querySelectorAll(s);
+}
+
+function _(t) {
+    var tagName, id, classes = [];
+    if (t.includes("#")) {
+        t = t.split('#');
+        tagName = t[0];
+        var b = t[1];
+        if (b.includes('.')) {
+            b = b.split('.');
+            id = b[0];
+            for (var i = 1; i < b.length; i++) {
+                classes.push(b[i]);
+            }
+        } else {
+            id = b;
+        }
+        //console.log(tagName)
+        //console.log(id)
+        //console.log(classes)
+    } else if (t.includes(".")) {
+        t = t.split('.');
+        tagName = t[0];
+        for (var i = 1; i < t.length; i++) {
+            classes.push(t[i]);
+        }
+        //console.log(tagName)
+        //console.log(classes)
+    } else {
+        tagName = t;
+    }
+
+    var e = document.createElement(tagName);
+    for (var i = 0; i < classes.length; i++) {
+        e.classList.add(classes[i]);
+    }
+    if (id) {
+        e.id = id
+    }
+    ;return e;
+}
+
+console.log("presets.js loaded.");
