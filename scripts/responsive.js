dc79b67c51af851e23bb6ec0937f70db117902bf
Elias Faisal <60409013+eliasfaisal@users.noreply.github.com>
2022-08-07 01:33:15 +0000

    Add files via upload

---
+++ scripts/responsive.js
@@ -0,0 +1,29 @@
+/* 
+    Created: 18 Feb 2022
+    Updated:  2 Jul 2022
+    Author : Elias Faisal
+             http://github.com/eliasfaisal/
+    License: CC0-1.0 license
+ */
+
+onresize = ()=>{
+    $("#main-widget > .content").style.height = innerHeight - 50 + "px";
+    $("#search-input").style.width = innerWidth - 104 + "px";
+    //
+    let w = parseInt(innerWidth / 346) * 346;
+    if(w < 342*2 || innerWidth < 342*2){
+        $("#files-conainer").style.textAlign = "center";
+    }else{
+        $("#files-conainer").style.textAlign = "";
+    }
+    if(w == 0 || w < 346 || innerWidth < 346){
+        $("#files-conainer").style.marginLeft = "0px";
+        $("#files-conainer").style.marginRight = "0px";
+    }else{
+        let margin = (innerWidth - w)/2 - 20;
+        $("#files-conainer").style.marginLeft = margin + "px";
+        $("#files-conainer").style.marginRight = margin + "px";
+    }
+    
+}
+onresize();
